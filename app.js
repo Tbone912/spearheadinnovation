fea17742d2de31edee5fb013d36d153dcd0f5661
Tbone912 <56204801+Tbone912@users.noreply.github.com>
2024-04-27 00:45:02 -0400

    added services page, added menu

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const dropdowns = document.querySelectorAll('.dropdown');
-    dropdowns.forEach(dropdown => {
-      dropdown.addEventListener('click', function(event) {
-        event.stopPropagation();
-        this.querySelector('.dropdown-content').classList.toggle('show');
-      });
-    });
-  
-    window.addEventListener('click', function(event) {
-      dropdowns.forEach(dropdown => {
-        dropdown.querySelector('.dropdown-content').classList.remove('show');
-      });
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdowns = document.querySelectorAll(".dropdown");
+  dropdowns.forEach((dropdown) => {
+    dropdown.addEventListener("click", function (event) {
+      event.stopPropagation();
+      this.querySelector(".dropdown-content").classList.toggle("show");
     });
   });
+
+  window.addEventListener("click", function (event) {
+    dropdowns.forEach((dropdown) => {
+      dropdown.querySelector(".dropdown-content").classList.remove("show");
+    });
+  });
+});
+
+function toggleAccordion(accordion) {
+  accordion.classList.toggle("active");
+  var panel = accordion.nextElementSibling;
+  if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+  } else {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+  }
+}
